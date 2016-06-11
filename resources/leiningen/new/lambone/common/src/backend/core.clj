10c05c0493664dbf990e54e8284039080cbc3f6f
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-06-10 18:11:01 -0700

    Add some minimal boilerplate for -main arg handling

--- resources/leiningen/new/lambone/common/src/backend/core.clj
+++ resources/leiningen/new/lambone/common/src/backend/core.clj
@@ -3,6 +3,7 @@
   (:require [mount.core :as mount]
             [clojure.tools.logging :as log]
             [clojure.tools.cli :as cli]
+            [clojure.string :as string]
             [robert.hooke :as hooke]
             [<<project-ns>>.system :as system]
             [<<project-ns>>.logging :as logging])
@@ -27,13 +28,37 @@
   (.addShutdownHook (Runtime/getRuntime) (Thread. #(system/stop)))
   (apply f args))
 
+(defn exit [status msg]
+  (.println *err* msg)
+  (System/exit status))
+
+(defn usage [options-summary]
+  (println (str "Options:\n" options-summary)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join "\n  " (cons "" errors))
+       "\n"))
+
 (def cli-options
   "Customize at will"
-  [#_["-p" "--port PORT" "Port number"
-      :parse-fn #(Integer/parseInt %)]])
+  [#_["-r" "--rollback" nil "Rollback database tables."
+      :id :rollback
+      :default false]
+   #_["-m" "--migrate" nil "Migrate database tables."
+      :id :migrate
+      :default false]
+   ["-h" "--help"]])
 
 (defn -main
   [& args]
   (hooke/add-hook #'<<project-ns>>.system/stop #'<<project-ns>>.core/main-stop)
   (hooke/add-hook #'<<project-ns>>.system/start #'<<project-ns>>.core/main-start)
-  (system/start (cli/parse-opts cli-options)))
+  (let [{:keys [options
+                arguments
+                errors
+                summary] :as args} (cli/parse-opts args cli-options)]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      errors (exit 1 (error-msg errors))
+      true (system/start args))))
